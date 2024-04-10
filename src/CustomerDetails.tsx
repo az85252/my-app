2a76be1da120616f2843c9a87cff2b4028a97fd8
az85252 <albertz4@illinois.edu>
2024-04-09 19:33:53 -0500

    Refactored code

--- src/CustomerDetails.tsx
+++ src/CustomerDetails.tsx
@@ -3,23 +3,19 @@ import { useParams } from 'react-router-dom';
 import { Customer } from './Customer';
 import './App.css'
 
-
 function CustomerDetails() {
 
-  const { id } = useParams();
+  const { id } = useParams(); //we need this to get the id for bwloe 
   const [customer, setUser] = useState<Partial<Customer>>({});
 
   useEffect(() => {
-    console.log("TRY")
     const fetchData = async () => {
       const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
       const data = await response.json();
       setUser(data);
-      console.log(data)
     };
     fetchData();
   }, []);
-
 
   return (
     <div>
@@ -27,19 +23,23 @@ function CustomerDetails() {
     <div>
       <p>Email: {customer.email}</p>
       <p>Phone: {customer.phone}</p>
-      {customer.address && (
       <div>
         <h4>Address:</h4>
-        <p>Street: {customer.address.street}</p>
-        <p>Suite: {customer.address.suite}</p>
-        <p>City: {customer.address.city}</p>
-        <p>Zipcode: {customer.address.zipcode}</p>
-
-      </div>)}
-      <div>
-          <h4>Geo:</h4>
+        <p>Street: {customer.address?.street}</p>
+        <p>Suite: {customer.address?.suite} </p> {/* Would Suite be optional?*/}
+        <p>City: {customer.address?.city}</p>
+        <p>Zipcode: {customer.address?.zipcode}</p>
+        <div>
+          <h5>Geo:</h5> {/*Would Geo also be optional or not?*/}
           <p>Lat: {customer.address?.geo?.lat}</p>
           <p>Lng: {customer.address?.geo?.lng}</p>
+        </div>
+      </div>
+      <div>
+        <h4>Company:</h4>
+        <p>Name: {customer.company?.name}</p>
+        <p>Catch Phrase: {customer.company?.catchPhrase}</p>
+        <p>BS: {customer.company?.bs}</p>
       </div>
     </div>
     </div>
@@ -48,7 +48,15 @@ function CustomerDetails() {
 
 export default CustomerDetails;
 
-
 //I decided that I wanted to solve problem 2 the similar way I did for problem one which is to use a state and useeffect hook. I feteched the api in a similar way to problem 1.
 //For the return I decided to list them out as divs or a list. I opted for using multiple div tags for each elemernt but if needed alternatively could use a loop internal for getting any key.
 //I also thought about that using a different route aka URL for this would be beneficial to seperate for getting specific customer.
+
+// If we wanted some parameters to be optional then it would look something like this:
+//  {customer.address && (
+//    <div>
+//    <h4>Address:</h4>
+//    <p>Street: {customer.address.street}</p>
+//    {customer.address.suite && 
+//      <p>Suite: {customer.address.suite}</p> } {/* Suite is optional */}
+//    <p>City: {customer.address.city}</p>
--- src/Customers.tsx
+++ src/Customers.tsx
@@ -1,19 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
-
 import { Customer } from './Customer';
 import './App.css'
 
 function Customers() {
-
   const [users, setUsers] = useState<Customer[]>([]);
 
   useEffect(() => {
-    console.log("try");
     const getData = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await response.json();
-    console.log(data + " Fetched ")
     setUsers(data);
   } 
   getData();
@@ -21,9 +17,9 @@ function Customers() {
 
 return(
   users.map(x => (
-    <div key={x.id ?? ''}>
-    <Link to={`/${x.id ?? ''}`}>
-      <p>{x.name ?? 'Unknown'}, {x.email ?? 'No Email Provided'}</p>
+    <div>
+    <Link to={`/${x.id}`}>
+      <p>{x.name}, {x.email}</p>
     </Link>
   </div>
   ) 
@@ -32,7 +28,6 @@ return(
 }
 
 export default Customers
-
 
 //As a Software engineer you are given a task to implement a simple UI that lists your company’s users, their details and posts.
 
